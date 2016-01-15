26d8efe42171d84ed96141c7026bb1e863e2a74d
navekazu <navekazu0927@gmail.com>
2016-01-15 17:34:23 +0900

    DBUnitを追加

---
+++ MyBatisSample/ddl.sql
@@ -0,0 +1,5 @@
+create table data_table (
+    id int,
+    value varchar(100),
+    primary key(id)
+);
--- MyBatisSample/src/test/java/tools/mybatis/sample/AppTest.java
+++ MyBatisSample/src/test/java/tools/mybatis/sample/AppTest.java
@@ -4,9 +4,15 @@ package tools.mybatis.sample;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.excel.XlsDataSet;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -15,9 +21,24 @@ import java.io.InputStream;
  */
 public class AppTest {
     private SqlSessionFactory sqlSessionFactory;
+    private IDatabaseTester databaseTester;
 
     @Before
-    public void setup() throws IOException {
+    public void setup() throws Exception {
+        // DBUnit setup
+        databaseTester = new JdbcDatabaseTester(
+            "org.h2.Driver",
+            "jdbc:h2:file:testdb/testdb;DB_CLOSE_ON_EXIT=FALSE",
+            "sa", // user
+            ""    // pass
+        );
+        databaseTester.setDataSet(
+            new XlsDataSet(new File("TestData.xls"))
+        );
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.onSetup();
+
+        // MyBatis setup
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
