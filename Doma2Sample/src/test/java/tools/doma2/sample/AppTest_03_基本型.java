d073ad4f68d17b9310ab8f7e93d70791d3ba8a6d
navekazu <navekazu0927@gmail.com>
2016-02-18 17:11:31 +0900

    Domaの自動生成まわりのIntelliJの設定方法をコメントに記入。

--- Doma2Sample/src/test/java/tools/doma2/sample/AppTest_03_基本型.java
+++ Doma2Sample/src/test/java/tools/doma2/sample/AppTest_03_基本型.java
@@ -5,6 +5,21 @@ import org.junit.*;
 import tools.doma2.sample.dao.EmployeeDao;
 import tools.doma2.sample.dao.EmployeeDaoImpl;
 
+/**
+ * 下記を参考にIntelliJでの設定を行った.
+ * http://d.hatena.ne.jp/taedium/20121229/p1
+ *
+ *   クラスパスの設定は不要だった
+ *
+ *   File -> Settingsの内容は、
+ *     「Store generated source relative to の項目で Module content root を選択」したうえで、
+ *       Production sources directory -> target/apt_generated
+ *       Test sources directory ->  target/apt_generated_test
+ *     とした。
+ *     （apt_generatedがpom.xmlに追加したDomaのビルドプラグインと同じになるようにした）
+ *
+ *   ソースフォルダの設定はDomaのビルドプラグインに合わせて、apt_generatedを見るようにした
+ */
 public class AppTest_03_基本型 extends AppTest {
     private IDatabaseTester databaseTester;
 
