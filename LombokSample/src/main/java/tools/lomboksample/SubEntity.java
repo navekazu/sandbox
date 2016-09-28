962a6dbc402c24ef0cebed5bb4c5178d1a50ada0
navekazu <navekazu0927@gmail.com>
2016-09-28 16:44:35 +0900

    Builderを追加。（テスト通らない気が）

--- LombokSample/src/main/java/tools/lomboksample/SubEntity.java
+++ LombokSample/src/main/java/tools/lomboksample/SubEntity.java
@@ -4,10 +4,12 @@ package tools.lomboksample;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Builder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SubEntity extends SuperEntity {
     private int field02;
 }
