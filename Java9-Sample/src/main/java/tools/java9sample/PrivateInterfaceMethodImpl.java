a27a3676120126e3a63ed16f3fe1491f8d96d697
k-watanabe <kazuo.watanabe@opentone.co.jp>
2017-10-06 11:02:53 +0900

    Java9Sample

---
+++ Java9-Sample/src/main/java/tools/java9sample/PrivateInterfaceMethodImpl.java
@@ -0,0 +1,9 @@
+package tools.java9sample;
+
+public class PrivateInterfaceMethodImpl implements PrivateInterfaceMethod {
+    public void publicMethod() {
+    }
+//    public void publicMethod() {
+//        privateMethod();
+//    }
+}
--- Java9-Sample/src/main/java/tools/java9sample/TryWithResources.java
+++ Java9-Sample/src/main/java/tools/java9sample/TryWithResources.java
@@ -2,18 +2,23 @@ package tools.java9sample;
 
 public class TryWithResources {
     public class Resource implements AutoCloseable {
-        public void throwableMethod() throws Exception {
+        public void throwExceptionMethod() throws Exception {
         }
         public void close() {
         }
     }
 
     public void tryWithResourcesSample() {
-//        Resource r1 = new Resource();
-//        try (Resource r2 = r1) {
-//        }
-        try (Resource r1 = new Resource()) {
+        Resource r1 = new Resource();
+        try (Resource r2 = r1) {
         }
+
+        Resource r3 = new Resource();
+        try (r3) {
+        }
+
+//        try (Resource r1 = new Resource()) {
+//        }
     }
 
     public void main(String[] args) {
